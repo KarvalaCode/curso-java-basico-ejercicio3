277bd737a8b24665bed5f4d8779b15ab5f4c4f95
Sara <77838356+Karvala3@users.noreply.github.com>
2021-12-29 23:46:25 +0100

    Add files via upload

---
+++ JavaBasicoEjercicio3/src/net/Karvala/BucleTexto.java
@@ -0,0 +1,22 @@
+package net.Karvala;
+
+public class BucleTexto {
+
+    public static void main(String[] args) {
+
+       String[] bienvenida = {"Recibe la bienvenida ", "al curso de ", "Java Básico ", "de los fantásticos chicos ", "de OpenBootCamp"};
+
+
+        for(int i = 0; i < bienvenida.length; i++) {
+
+            System.out.print(bienvenida[i]);
+
+        }
+    }
+
+
+
+
+
+
+}
